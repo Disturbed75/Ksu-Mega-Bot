4e5b20622d0f41fcbe4a135d4f7def7737c3d7e5
Disturbed75 <sashafilth@gmail.com>
2023-05-06 23:36:56 +0300

    fix module

--- bot.py
+++ bot.py
@@ -9,13 +9,14 @@ BASE_URL = "https://api.telegram.org/bot{}".format(BOT_TOKEN)
 
 def handler(event, context):
     try:
+        http = urllib3.PoolManager()
         income = json.loads(event["body"])
         chat_id = income["message"]["chat"]["id"]
         data = {"text": texts.welcome_message, "chat_id": chat_id}
         body = json.dumps(data)
         url = BASE_URL + "/sendMessage"
-        print(BASE_URL)
-        urllib3.request("POST", url, body)
+        print(url)
+        http.request("POST", url, body)
     except Exception as e:
         print(str(e))
     return {"statusCode": 200}
