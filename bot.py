25b6de964c3468470f9441c3ba788aff8d0c9f8f
Disturbed75 <sashafilth@gmail.com>
2023-05-06 23:42:34 +0300

    fix urllib3

--- bot.py
+++ bot.py
@@ -7,16 +7,16 @@ import urllib3
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BASE_URL = "https://api.telegram.org/bot{}".format(BOT_TOKEN)
 
+
 def handler(event, context):
     try:
         http = urllib3.PoolManager()
         income = json.loads(event["body"])
         chat_id = income["message"]["chat"]["id"]
-        data = {"text": texts.welcome_message, "chat_id": chat_id}
-        body = json.dumps(data)
+        response = {"text": texts.welcome_message, "chat_id": chat_id}
+        encoded_body = json.dumps(response)
         url = BASE_URL + "/sendMessage"
-        print(url)
-        http.request("POST", url, body)
+        http.request('POST', url, headers={'Content-Type': 'application/json'}, body=encoded_body)
     except Exception as e:
         print(str(e))
     return {"statusCode": 200}
