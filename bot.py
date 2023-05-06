cb1a5754a7070934d09380265817d2b2b7cb73bd
Disturbed75 <sashafilth@gmail.com>
2023-05-06 23:35:13 +0300

    ad urllib3

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 import os
 import json
 import texts
-from botocore.vendored import requests
+import urllib3
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BASE_URL = "https://api.telegram.org/bot{}".format(BOT_TOKEN)
@@ -11,11 +11,11 @@ def handler(event, context):
     try:
         income = json.loads(event["body"])
         chat_id = income["message"]["chat"]["id"]
-        print(chat_id)
         data = {"text": texts.welcome_message, "chat_id": chat_id}
-        url = BASE_URL + '/sendMessage'
+        body = json.dumps(data)
+        url = BASE_URL + "/sendMessage"
         print(BASE_URL)
-        requests.post(url, data)
+        urllib3.request("POST", url, body)
     except Exception as e:
         print(str(e))
     return {"statusCode": 200}
